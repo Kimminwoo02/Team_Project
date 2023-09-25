b5ee42b4a14a72e2c906776e84e617f40f4b3eb6
minutaurus <made_power@naver.com>
2023-09-25 16:38:38 +0900

    알림, 캘린더 등 프론트 화면 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- insert into member(id,email,password,name,phone,address,gender)
--- values (1,'made_power@naver.com','1234','미누짱','010-1234-1234','서울시동작구대방동',1);
+-- insert into member(member_id,email,password,name,phone,gender)
+-- values (,'made_power@naver.com','1234','미누짱','010-1234-1234',1);
 --
 -- /*,now(), 'Minu', now(), 'Minu'*/

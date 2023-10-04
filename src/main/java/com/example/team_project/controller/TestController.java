7f03a32879c86f2e5d0a59ab3fe035ce88834ea2
KT <clouderneering@gmail.com>
2023-10-04 17:23:42 +0900

    Package import runtime error 버그 픽스

--- src/main/java/com/example/team_project/controller/TestController.java
+++ src/main/java/com/example/team_project/controller/TestController.java
@@ -1,10 +1,17 @@
 package com.example.team_project.controller;
 
+import com.example.team_project.dto.ReviewDto;
+import com.example.team_project.entity.Review;
+import com.example.team_project.security.CustomUserDetails;
 import com.example.team_project.service.MemberServiceMybatis;
+import com.example.team_project.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,9 +21,7 @@ import java.util.List;
 @Slf4j
 public class TestController {
 
-    private final MemberServiceMybatis memberServiceMybatis;
-
-
+    private final ReviewService reviewService;
 
     @GetMapping("/home")
     public String home(Model model){
@@ -59,7 +64,7 @@ public class TestController {
 
     }
 
-    @PostMapping("/review")
+    @PostMapping ("/review")
     public String review(ReviewDto reviewDto, @AuthenticationPrincipal CustomUserDetails principal) {
         reviewDto.setMember(principal.getMember());
         reviewService.addReview(reviewDto);

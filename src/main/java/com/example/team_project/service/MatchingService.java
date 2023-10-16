08ca4310f8a873c388b9b42c339d79087eb60a81
MonsterEner <clouderneering@gmail.com>
2023-10-16 22:39:33 +0900

    Matching 기능 업데이트(미완)

---
+++ src/main/java/com/example/team_project/service/MatchingService.java
@@ -0,0 +1,14 @@
+package com.example.team_project.service;
+
+import com.example.team_project.dto.MatchingDTO;
+import com.example.team_project.entity.matching.Matching;
+
+import java.util.List;
+
+public interface MatchingService {
+
+    void createMatching(MatchingDTO matchingDTO);
+
+    List<Matching> findAll();
+
+}
